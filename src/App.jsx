1249fa02d39694645bf3b63c9b50b55e9a260fb6
anibal-alpizar <anibal.alpizar14@gmail.com>
2022-10-22 17:27:53 -0600

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import BtnMenuMovil from "./components/BtnMenuMovil";
+import Content from "./components/Content";
 import Logo from "./components/Logo";
 import Logout from "./components/Logout";
 import Nav from "./components/Nav";
@@ -10,7 +11,7 @@ function App() {
     setshowMenu(!showMenu);
   };
   return (
-    <div className="">
+    <div className="bg-[#141517]">
       <sidebar
         className={`bg-[#1E1F24] fixed ${
           showMenu ? "-left-0" : "-left-full"
@@ -23,6 +24,7 @@ function App() {
         <Logout />
       </sidebar>
       <BtnMenuMovil toggleMenu={toggleMenu} showMenu={showMenu} />
+      <Content/>
     </div>
   );
 }

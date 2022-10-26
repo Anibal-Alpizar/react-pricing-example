e05c9d78eabc2e8f1ed23a055e73fb744e64315b
anibal-alpizar <anibal.alpizar14@gmail.com>
2022-10-25 22:58:07 -0600

    finished project

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -6,9 +6,9 @@ function Card(props) {
       <div className={`${props.color} "p-12 rounded-xl mb-10>`}>
         <div className="flex flex-col items-center mb-20">
           {props.icon}
-          <h2 className="text-4xl">{props.plan}</h2>
-          <p className="text-gray-500 mb-4">{props.description}</p>
-          <h5 className="text-2xl uppercase tracking-[4px] font-bold">
+          <h2 className={`${props.colorWhite} text-4xl">`}>{props.plan}</h2>
+          <p className={`${props.colorDesc} mb-4">`}>{props.description}</p>
+          <h5 className={`${props.colorWhite} text-2xl uppercase tracking-[4px] font-bold `}>
             {props.price}
           </h5>
           <p className="text-gray-500 mb-8">{props.for}</p>
--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -1,23 +1,46 @@
-import { FcGlobe, FcBiohazard } from "react-icons/fc";
+import { FcGlobe, FcBiohazard, FcElectroDevices } from "react-icons/fc";
 import Card from "./Card";
 
 function Plans() {
   return (
     <div className="text-gray-300 grid grid-cols-3 gap-8">
       <Card
-        icon={<FcGlobe className="text-7xl my-6 p-4 bg-purple-50 rounded-full " />}
+        icon={
+          <FcGlobe className="text-7xl my-6 p-4 bg-purple-50 rounded-full " />
+        }
         plan="Personal"
         description="Perfect plan for starters"
         price="Free"
         for="For a Lifetime"
         details="lore ipsum dolor sit amet"
         color="bg-[#1E1F24] "
+        colorDesc="text-gray-500"
       />
       <Card
-        icon={<FcBiohazard className="text-7xl my-6 p-4 bg-purple-50 rounded-full" />}
+        icon={
+          <FcBiohazard className="text-7xl my-6 p-4 bg-purple-50 rounded-full" />
+        }
+        plan="Personal"
+        description="Perfect plan for starters"
+        price="Free"
+        for="For a Lifetime"
+        details="lore ipsum dolor sit amet"
         color="bg-[#664EFA]"
+        colorDesc="text-gray-300"
+        colorWhite="text-white"
       />
-      <Card />
+      <Card
+        icon={
+          <FcElectroDevices className="text-7xl my-6 p-4 bg-purple-50 rounded-full" />
+        }
+        plan="Personal"
+        description="Perfect plan for starters"
+        price="Free"
+        for="For a Lifetime"
+        details="lore ipsum dolor sit amet"
+        color="bg-[#1E1F24] "
+        colorDesc="text-gray-500"
+      />
     </div>
   );
 }
